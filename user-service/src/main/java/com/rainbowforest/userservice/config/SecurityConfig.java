1edec5eae09ac9f5910dc2b3dcac0bc228213204
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2025-04-20 09:51:52 +0700

    done be user-service and tested by postman

---
+++ user-service/src/main/java/com/rainbowforest/userservice/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.rainbowforest.userservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/registration").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+}
