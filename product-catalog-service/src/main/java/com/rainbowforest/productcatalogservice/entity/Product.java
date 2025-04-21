b42839964f32473d852761f9549b217e51d776f4
LAPTOP-GBK77T8O\Asus <levietnguyenhung0711@gmail.com>
2025-04-21 21:11:32 +0700

    fix model product be

--- product-catalog-service/src/main/java/com/rainbowforest/productcatalogservice/entity/Product.java
+++ product-catalog-service/src/main/java/com/rainbowforest/productcatalogservice/entity/Product.java
@@ -1,40 +1,43 @@
 package com.rainbowforest.productcatalogservice.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Entity
-@Table (name = "products")
+@Table(name = "products")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Product {
 
-    @Id
-    @GeneratedValue (strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column (name = "product_name")
-    @NotNull
-    private String productName;
+	@Column(name = "product_name")
+	@NotNull
+	private String productName;
 
-    @Column (name = "price")
-    @NotNull
-    private BigDecimal price;
+	@Column(name = "price")
+	@NotNull
+	private BigDecimal price;
 
-    @Column (name = "discription")
-    private String discription;
+	@Column(name = "discription") // Consider correcting to "description"
+	private String discription;
 
-    @Column (name = "category")
-    @NotNull
-    private String category;
+	@Column(name = "category")
+	@NotNull
+	private String category;
 
-    @Column (name = "availability")
-    @NotNull
-    private int availability;
+	@Column(name = "quantity")
+	@NotNull
+	private int quantity;
+
+	@Column(name = "image_url")
+	private String imageUrl;
 
 	public Product() {
-
 	}
 
 	public Long getId() {
@@ -77,11 +80,19 @@ public class Product {
 		this.category = category;
 	}
 
-	public int getAvailability() {
-		return availability;
+	public int getQuantity() {
+		return quantity;
 	}
 
-	public void setAvailability(int availability) {
-		this.availability = availability;
-	} 
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
 }
